86dbd4dfe32d563afee51fe5c474f820005dcd75
Mariano Garcia Inza <mariano.garciainza@gmail.com>
2023-05-26 19:07:04 -0300

    hice retoques al archivo de python

--- sample.py
+++ sample.py
@@ -1,3 +1,10 @@
 #script de pyton de prueba
 a="!"
 print(f"hola{a}")
+
+for i in range (10):
+    print(2*i, end=') ')
+    for j in range(11):
+        print(f'{i},{j} ', end=' ')
+    print('')
+print("fin")
